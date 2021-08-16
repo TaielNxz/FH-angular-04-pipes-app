86c932c1e8992d396fe940cb51452bb7b7412999
TaielNxz <TaielNxz@hotmail.com>
2021-08-16 12:35:17 -0300

    initial commit

---
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-no-comunes',
+  templateUrl: './no-comunes.component.html',
+  styles: [
+  ]
+})
+export class NoComunesComponent {
+
+  // i18nSelect
+  nombre: string = 'Fernando';
+  genero: string = 'masculino';
+  invitacionMapa = {
+    'masculino' : 'invitarlo',
+    'femenino'  : 'invitarla'
+  }
+
+    // i18nPlural
+    clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Hernando', 'Eduardo', 'Fernando'];
+    clientesMapa = {
+      '=0' : 'no tenemos ningun cliente esperando.',
+      '=1' : 'tenemos un cliente esperando.',
+      '=2' : 'tenemos 2 clientes esperando.',
+      'other' : 'tenemos # clientes esperando'
+    }
+
+    cambiarCliente() {
+      this.nombre = 'Melissa';
+      this.genero = 'femenino';
+    }
+
+    borrarCliente() {
+      this.clientes.pop();
+    }
+
+    // keyValue Pipe
+    persona = {
+      nombre: 'Fernando',
+      edad: 35,
+      direccion: 'Ottawa, Canada'
+    }
+
+    // JsonPipe
+    heroes = [
+      {
+        nombre: 'Superman',
+        vuela: true
+      },
+      {
+        nombre: 'Robin',
+        vuela: false
+      },
+      {
+        nombre: 'Aquaman',
+        vuela: false
+      }
+    ]
+
+    // Async Pipe
+    miObservable = interval(2000); // 0,1,2,3,4,5,6,
+
+    valorPromesa = new Promise( (resolve, reject) => {
+
+      setTimeout(() => {
+        resolve( 'Tenemos data de la promesa' )
+      }, 3500);
+
+    });
+
+}
